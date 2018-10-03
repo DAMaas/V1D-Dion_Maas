4549e9abb5b93a1c850825b5827561664c59a777
Dion Maas <dion.maas@gmail.com>
2018-10-03 09:04:38 +0200

    NS Project First

--- Homework/Miniproject NS/Miniproject NS.py
+++ Homework/Miniproject NS/Miniproject NS.py
@@ -1,10 +1,12 @@
 # Miniproject NS
 
-afstandKM = 123
-leeftijd = 69
-weekendrit = True
+afstandKM = 0
+leeftijd = 0
+weekendrit = False
+magLeeftijdKorting = leeftijd < 12 or leeftijd >= 65
 
 def standaardprijs(afstandKM):
+    prijs = 0
     prijsPerKM = 0.80
     vastBedragVanafVijftigKM = 15
     goedkoperePrijsVanafVijftigPerKM = 0.60
@@ -20,16 +22,33 @@ def ritprijs(leeftijd, weekendrit, afstandKM):
     basisPrijs = standaardprijs(afstandKM)
     kortingPercentage = 0
 
-    if leeftijd < 12 or leeftijd >= 65:
-        if weekendrit == True:
+    if magLeeftijdKorting:
+        if weekendrit:
             kortingPercentage = 35
         else:
             kortingPercentage = 30
-    elif weekendrit == True:
+    elif weekendrit:
         kortingPercentage = 40
 
     kortingPrijs = basisPrijs - ((basisPrijs / 100)*kortingPercentage)
     return kortingPrijs
 
-print(standaardprijs(afstandKM))
-print(ritprijs(leeftijd, weekendrit, afstandKM))
+print(ritprijs(11, False, 100))
+print(ritprijs(12, False, 200))
+print(ritprijs(64, False, 300))
+print(ritprijs(65, False, 400))
+
+print(ritprijs(11, True, 100))
+print(ritprijs(12, True, 200))
+print(ritprijs(64, True, 300))
+print(ritprijs(65, True, 400))
+
+print(ritprijs(11, False, -100))
+print(ritprijs(12, False, -200))
+print(ritprijs(64, False, 3))
+print(ritprijs(65, False, 4))
+
+print(ritprijs(11, True, 1))
+print(ritprijs(12, True, 2))
+print(ritprijs(64, True, -300))
+print(ritprijs(65, True, -400))
